b7dfc2ad2e282193fcb8ca59904ea8794f62a485
Mikahil Sokolov <pilligrim17@yandex.ru>
2020-06-13 12:25:43 +0300

    fixed after review

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,25 @@
 const popup = document.querySelector('.popup');
 
 const popup__container = document.querySelector('.popup__container');
-const nameInput = document.querySelector('#popup__input_field_name');
-const jobInput  = document.querySelector('#popup__input_field_status');
+const nameInput = document.querySelector('.popup__input_field_name');
+const jobInput  = document.querySelector('.popup__input_field_status');
 const btnPopupClose = document.querySelector('.popup__close');
 
 const profileName = document.querySelector('.profile__name');
 const profileStatus  = document.querySelector('.profile__status');
 const btnProfileEdit = document.querySelector('.profile__edit-btn');
 
+/*
+В практической работе есть два требования:
+- DOM-элементы, к которым есть обращение в скрипте, вынесены в константы.
+- Переменные объявлены через let.
+У меня объекты (выше) в константах, а "обычных" переменных нет вообще. Поэтому без let
+*/
+
 function openPopupEdit() {
   popup.classList.add('popup_opened');
+  // В описании к работе тут предполагают сохранить значения со страницы в переменные,
+  // а потом уже их присваивать в поля для ввода. Не вижу в этом смысла
   nameInput.value = profileName.textContent;
   jobInput.value = profileStatus.textContent;
 }
@@ -25,7 +34,7 @@ function formSubmitHandler (evt) {
 
     profileName.textContent = nameInput.value;
     profileStatus.textContent = jobInput.value;
-    popup.classList.remove('popup_opened');
+    closePopupEdit();
 }
 
 btnProfileEdit.addEventListener('click',openPopupEdit);
