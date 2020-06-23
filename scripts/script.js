6cab4dcbb9d0b66da38d5a8eb23cd66c6fa2ef99
Mikahil Sokolov <pilligrim17@yandex.ru>
2020-06-23 23:26:33 +0300

    newFeat: addCard() and initial cards on start

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,32 @@
 
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
 const popup = document.querySelector('.popup');
 
 const popup__container = document.querySelector('.popup__container');
@@ -10,6 +38,7 @@ const profileName = document.querySelector('.profile__name');
 const profileStatus  = document.querySelector('.profile__status');
 const btnProfileEdit = document.querySelector('.profile__edit-btn');
 
+const cardsContainer = document.querySelector('.elements');
 /*
 В практической работе есть два требования:
 - DOM-элементы, к которым есть обращение в скрипте, вынесены в константы.
@@ -40,3 +69,16 @@ function formSubmitHandler (evt) {
 btnProfileEdit.addEventListener('click',openPopupEdit);
 btnPopupClose.addEventListener('click',closePopupEdit);
 popup__container.addEventListener('submit', formSubmitHandler);
+
+function addCard(title, image) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const newCard = cardTemplate.cloneNode(true);
+  newCard.querySelector('.elements__image').src = image;
+  newCard.querySelector('.elements__image').alt = title;
+  newCard.querySelector('.elements__title').textContent  = title;
+
+  cardsContainer.append(newCard);
+}
+
+
+initialCards.forEach(item => addCard(item.name, item.link));
